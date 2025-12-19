d7b9a2e5bdaaa79892ad2707e118959c6c4688a7
apala-1 <apalalamichhane13579@gmail.com>
2025-12-19 09:34:20 +0545

    Task again

--- app/example/react-hook-form/page.tsx
+++ app/example/react-hook-form/page.tsx
@@ -8,7 +8,24 @@ export default function Page() {
     } = useForm(
         { values: { email: "", password: "" } } // input states and initial
     );
+
+    const onSubmit = async ( data: any ) => {  // data formdata
+        alert(data.email);    
+    }
     return (
-        <div></div>
+        <div>
+            <form onSubmit={ handleSubmit(onSubmit) }
+            className="mx-auto p-2 max-w-xl border">
+                <div className="mt-2">
+                    <label>Username</label>
+                    <input { ...register("email", { required: "Email hala" }) } />
+                    {
+                        errors.email && // conditional rendering
+                        <span className="text-red-500">{ errors.email.message }</span>
+                    }
+                </div>
+                <button type="submit" className="p-2 bg-green-500">Submit</button>
+            </form>
+        </div>
     );
 }
